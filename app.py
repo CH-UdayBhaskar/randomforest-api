15a2010e51ad084110d5297308745e39d67aaeba
CH-UdayBhaskar <cherukuriudaybhaskar@gmail.com>
2025-07-15 17:53:48 +0530

    features updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from fastapi.responses import RedirectResponse
 from typing import Dict
 import numpy as np
@@ -15,15 +15,32 @@ model = joblib.load("model.pkl")
 #     features: list[float]
 
 class Input(BaseModel):
-    features: Dict[str, float]
+    cycle_index: float = Field(..., alias="Cycle_Index")
+    discharge_time: float = Field(..., alias="Discharge Time (s)")
+    decrement: float = Field(..., alias="Decrement 3.6-3.4V (s)")
+    max_voltage: float = Field(..., alias="Max. Voltage Dischar. (V)")
+    min_voltage: float = Field(..., alias="Min. Voltage Charg. (V)")
+    time_at_4_15v: float = Field(..., alias="Time at 4.15V (s)")
+    time_constant: float = Field(..., alias="Time constant current (s)")
+    charging_time: float = Field(..., alias="Charging time (s)")
 
-column_names=["Cycle_Index",	"Discharge Time (s)",	"Decrement 3.6-3.4V (s)",	"Max. Voltage Dischar. (V)",	"Min. Voltage Charg. (V)",	"Time at 4.15V (s)",	"Time constant current (s)",	"Charging time (s)"]
+    class Config:
+        allow_population_by_field_name = True  # enables alias usage
 
 @app.post("/predict")
 def predict(input: Input):
     #data = np.array([input.features])
-    values=[input.features[col] for col in column_names]
-    data = np.array([values])
+    #values=[input.features[col] for col in column_names]
+    data = np.array([[
+        input.cycle_index,
+        input.discharge_time,
+        input.decrement,
+        input.max_voltage,
+        input.min_voltage,
+        input.time_at_4_15v,
+        input.time_constant,
+        input.charging_time
+    ]])
     prediction = model.predict(data)
     return {"prediction": int(prediction[0])}
 
