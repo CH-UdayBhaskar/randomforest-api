403a84c7ced0469fda9634bb1db5b7cd25f0f092
CH-UdayBhaskar <cherukuriudaybhaskar@gmail.com>
2025-07-15 11:32:21 +0530

    Intial Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import numpy as np
+import joblib
+
+app = FastAPI()
+
+model = joblib.load("model.pkl")
+
+class Input(BaseModel):
+    features: list[float]
+
+@app.post("/predict")
+def predict(input: Input):
+    data = np.array([input.features])
+    prediction = model.predict(data)
+    return {"prediction": int(prediction[0])}
