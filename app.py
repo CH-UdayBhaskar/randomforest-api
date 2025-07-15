eb20851f577a142a6dcde19271e5f26a30b8483c
CH-UdayBhaskar <cherukuriudaybhaskar@gmail.com>
2025-07-15 16:06:57 +0530

    uvicorn updated

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
 import joblib
+import uvicorn
+import os
 
 app = FastAPI()
 
@@ -15,3 +17,11 @@ def predict(input: Input):
     data = np.array([input.features])
     prediction = model.predict(data)
     return {"prediction": int(prediction[0])}
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to RandomForest API! Visit /docs"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
