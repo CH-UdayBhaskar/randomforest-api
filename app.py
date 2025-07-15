23dc605a1e6f2b0140e4457186646c62a4e0181d
CH-UdayBhaskar <cherukuriudaybhaskar@gmail.com>
2025-07-15 17:01:58 +0530

    updated_1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import RedirectResponse
+from typing import Dict
 import numpy as np
 import joblib
 import uvicorn
@@ -14,28 +15,15 @@ model = joblib.load("model.pkl")
 #     features: list[float]
 
 class Input(BaseModel):
-    Cycle_Index: float
-    Discharge Time (s): float
-    Decrement 3.6-3.4V (s): float
-    Max. Voltage Dischar. (V): float
-    Min. Voltage Charg. (V): float
-    Time at 4.15V (s): float
-    Time constant current (s): float
-    Charging time (s): float
+    features: Dict[str, float]
 
+column_names=["Cycle_Index",	"Discharge Time (s)",	"Decrement 3.6-3.4V (s)",	"Max. Voltage Dischar. (V)",	"Min. Voltage Charg. (V)",	"Time at 4.15V (s)",	"Time constant current (s)",	"Charging time (s)"]
 
 @app.post("/predict")
 def predict(input: Input):
     #data = np.array([input.features])
-    data = np.array([[
-        input.Cycle_Index, 
-        input.Discharge Time (s), 
-        input.Decrement 3.6-3.4V (s), 
-        input.Max. Voltage Dischar. (V),
-        input.Min. Voltage Charg. (V),
-        input.Time at 4.15V (s),
-        input.Time constant current (s),
-        input.Charging time (s)]])
+    values=[input.features[col] for col in column_names]
+    data = np.array([values])
     prediction = model.predict(data)
     return {"prediction": int(prediction[0])}
 
@@ -48,4 +36,4 @@ if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=port)
 
 
-Cycle_Index	Discharge Time (s)						
+						
