2e115401e6b81d8488b71afb04cdf3cb1324a3c2
CH-UdayBhaskar <cherukuriudaybhaskar@gmail.com>
2025-07-15 16:40:48 +0530

    updated

--- app.py
+++ app.py
@@ -10,12 +10,32 @@ app = FastAPI()
 
 model = joblib.load("model.pkl")
 
+# class Input(BaseModel):
+#     features: list[float]
+
 class Input(BaseModel):
-    features: list[float]
+    Cycle_Index: float
+    Discharge Time (s): float
+    Decrement 3.6-3.4V (s): float
+    Max. Voltage Dischar. (V): float
+    Min. Voltage Charg. (V): float
+    Time at 4.15V (s): float
+    Time constant current (s): float
+    Charging time (s): float
+
 
 @app.post("/predict")
 def predict(input: Input):
-    data = np.array([input.features])
+    #data = np.array([input.features])
+    data = np.array([[
+        input.Cycle_Index, 
+        input.Discharge Time (s), 
+        input.Decrement 3.6-3.4V (s), 
+        input.Max. Voltage Dischar. (V),
+        input.Min. Voltage Charg. (V),
+        input.Time at 4.15V (s),
+        input.Time constant current (s),
+        input.Charging time (s)]])
     prediction = model.predict(data)
     return {"prediction": int(prediction[0])}
 
@@ -26,3 +46,6 @@ def root():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app:app", host="0.0.0.0", port=port)
+
+
+Cycle_Index	Discharge Time (s)						
